e44aa0a6d193ab0f700b4b98888875f690fa293a
mark <markaguirre26@gmail.com>
2019-11-01 21:17:07 +0800

    first commit

---
+++ Blotter/frm_camera.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AppSystem
+{
+    partial class frm_camera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd_save = new System.Windows.Forms.Button();
+            this.cmd_reset = new System.Windows.Forms.Button();
+            this.cmd_capture = new System.Windows.Forms.Button();
+            this.imgCapture = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmd_save
+            // 
+            this.cmd_save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmd_save.Enabled = false;
+            this.cmd_save.Location = new System.Drawing.Point(147, 215);
+            this.cmd_save.Name = "cmd_save";
+            this.cmd_save.Size = new System.Drawing.Size(66, 23);
+            this.cmd_save.TabIndex = 9;
+            this.cmd_save.Text = "&OK";
+            this.cmd_save.UseVisualStyleBackColor = true;
+            this.cmd_save.Click += new System.EventHandler(this.cmd_save_Click);
+            // 
+            // cmd_reset
+            // 
+            this.cmd_reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmd_reset.Enabled = false;
+            this.cmd_reset.Location = new System.Drawing.Point(81, 215);
+            this.cmd_reset.Name = "cmd_reset";
+            this.cmd_reset.Size = new System.Drawing.Size(66, 23);
+            this.cmd_reset.TabIndex = 8;
+            this.cmd_reset.Text = "Reset";
+            this.cmd_reset.UseVisualStyleBackColor = true;
+            this.cmd_reset.Click += new System.EventHandler(this.cmd_reset_Click);
+            // 
+            // cmd_capture
+            // 
+            this.cmd_capture.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmd_capture.Location = new System.Drawing.Point(15, 215);
+            this.cmd_capture.Name = "cmd_capture";
+            this.cmd_capture.Size = new System.Drawing.Size(66, 23);
+            this.cmd_capture.TabIndex = 7;
+            this.cmd_capture.Text = "Capture";
+            this.cmd_capture.UseVisualStyleBackColor = true;
+            this.cmd_capture.Click += new System.EventHandler(this.cmd_capture_Click);
+            // 
+            // imgCapture
+            // 
+            this.imgCapture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgCapture.Location = new System.Drawing.Point(8, 9);
+            this.imgCapture.Name = "imgCapture";
+            this.imgCapture.Size = new System.Drawing.Size(224, 169);
+            this.imgCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgCapture.TabIndex = 11;
+            this.imgCapture.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 169);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 185);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // frm_camera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 253);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmd_save);
+            this.Controls.Add(this.cmd_reset);
+            this.Controls.Add(this.cmd_capture);
+            this.Controls.Add(this.imgCapture);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frm_camera";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Camera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frm_camera_FormClosing);
+            this.Load += new System.EventHandler(this.frm_camera_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd_save;
+        private System.Windows.Forms.Button cmd_reset;
+        private System.Windows.Forms.Button cmd_capture;
+        private System.Windows.Forms.PictureBox imgCapture;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Blotter/frm_camera.cs
@@ -0,0 +1,129 @@
+﻿using Camera_NET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Camera_NET;
+using DirectShowLib;
+using DPFP.Capture;
+using AForge.Video.DirectShow;
+using AForge.Video;
+
+
+namespace AppSystem
+{
+    public partial class frm_camera : Form
+    {
+
+
+        //Capture capture = new Capture();        
+        private FilterInfoCollection captureDevice;
+        private VideoCaptureDevice finalFrame;
+
+
+        public frm_camera()
+        {
+            InitializeComponent();
+            //capture.StartCapture();
+            //capture.EventHandler = this;
+        }
+
+
+        private void frm_camera_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            captureDevice = new FilterInfoCollection(AForge.Video.DirectShow.FilterCategory.VideoInputDevice);
+            foreach (AForge.Video.DirectShow.FilterInfo Device in captureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+
+            comboBox1.SelectedIndex = 0;
+            
+            finalFrame = new VideoCaptureDevice();
+            finalFrame = new VideoCaptureDevice(captureDevice[comboBox1.SelectedIndex].MonikerString);
+            finalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            finalFrame.Start();
+
+
+        }
+
+
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                imgCapture.Image = (Bitmap)eventArgs.Frame.Clone();
+            }
+            catch (Exception ex)
+            {
+
+                Console.Write(ex.Message);
+            }
+
+        }
+
+
+
+        private void frm_camera_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (finalFrame.IsRunning == true)
+            {
+                finalFrame.Stop();
+            }
+     
+        }
+
+       
+
+        private void cmd_capture_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Visible = true;
+            imgCapture.Visible = false;
+            pictureBox1.Image = imgCapture.Image;
+            cmd_reset.Enabled = true; cmd_save.Enabled = true; cmd_capture.Enabled = false;
+        }
+
+        private void cmd_reset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                pictureBox1.Visible = false;
+                imgCapture.Visible = true;
+                cmd_reset.Enabled = false; cmd_save.Enabled = false; cmd_capture.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+     
+
+        private void cmd_save_Click(object sender, EventArgs e)
+        {
+            var f = (Application.OpenForms["frm_user"] as frm_user);
+            //.Image = imgCapture.Image;      
+            if (f.wizard1.SelectedTab == f.tabPage3)
+            {
+                f.pbUser.Image = imgCapture.Image;
+            }
+            else
+            {
+                f.pbStudent.Image = imgCapture.Image;
+            }
+            pictureBox1.Visible = false;
+            imgCapture.Visible = true;
+            this.Close();
+        }
+
+    }
+}
